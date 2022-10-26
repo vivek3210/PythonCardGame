24c23f3af3abfc5ba4068a05c3014155db800f77
vivek3210 <36582094+vivek3210@users.noreply.github.com>
2022-10-26 12:54:18 -0400

    Added logic for shuffling cards and drawing them.

---
+++ GameLogic.py
@@ -0,0 +1,11 @@
+import itertools
+import random
+
+deck = list(itertools.product(range(1,2),['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']))
+
+random.shuffle(deck)
+# draws three cards
+print("You got:")
+for i in range(3):
+    print(deck[i][0], "of", deck[i][1])
+
