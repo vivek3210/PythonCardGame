cb87fad487137a76dd0eaef3d8085ab26cf04394
Ben Scholl <xpclippy@gmail.com>
2022-11-05 22:02:08 -0400

    Update GameLogic.py

--- GameLogic.py
+++ GameLogic.py
@@ -8,9 +8,9 @@ screen.fill('white')
 pygame.display.set_caption('We Suck At Naming Card Games')
 fps_clock = pygame.time.Clock()
 
-card1 = pygame.image.load('graphics/cards/3_of_clubs.png')
-card2 = pygame.image.load('graphics/cards/9_of_clubs.png')
-card3 = pygame.image.load('graphics/cards/5_of_clubs.png')
+card1render = pygame.image.load('graphics/cards/3_of_clubs.png')
+card2render = pygame.image.load('graphics/cards/9_of_clubs.png')
+card3render = pygame.image.load('graphics/cards/5_of_clubs.png')
 
 deck = list(
     itertools.product(range(1, 2), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
@@ -33,10 +33,14 @@ def get_cards():
     for i in range(3):
         print(deck[i][0], "of", deck[i][1])
 
+random.shuffle(deck)
 card1 = deck[0][0]
 card2 = deck[1][0]
 card3 = deck[2][0]
 
+print(card1)
+print(card2)
+print(card3)
 def get_shield(atk, defense):
     defense -= atk
     return defense
@@ -49,11 +53,9 @@ def get_hp(defense, hp):
         return hp
 
 
-get_cards()
 player_two_shield = get_shield(player_one_attack, player_two_shield)
 player_two_hp = get_hp(player_two_shield, player_two_hp)
 
-print(player_two_hp)
 
 while True:
     # actually runs game
@@ -63,9 +65,9 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(card1, (0, 0))
-    screen.blit(card2, (200, 0))
-    screen.blit(card3, (400, 0))
+    screen.blit(card1render, (0, 0))
+    screen.blit(card2render, (200, 0))
+    screen.blit(card3render, (400, 0))
 
     pygame.display.update()
     fps_clock.tick(60)
